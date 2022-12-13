6278187b788de8816f0fdfd27a4c16fe224f7ea6
Paweł Ryszawa <pablo53@poczta.onet.eu>
2022-12-13 03:01:22 +0100

    Open API 3 and Swagger UI

--- app/src/main/kotlin/net/pryszawa/micronautlab/app/Application.kt
+++ app/src/main/kotlin/net/pryszawa/micronautlab/app/Application.kt
@@ -1,7 +1,18 @@
 package net.pryszawa.micronautlab.app
 
 import io.micronaut.runtime.Micronaut.*
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 
+@OpenAPIDefinition(
+    info = Info(
+        title = "micronautlab",
+        version = "1.0.0"
+    )
+)
+object Api {
+
+}
 fun main(args: Array<String>) {
     run(*args)
 }
