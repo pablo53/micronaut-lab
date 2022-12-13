4ce3d13a5f969fae358e273673bc935b50da4317
Paweł Ryszawa <pablo53@poczta.onet.eu>
2022-12-13 02:15:29 +0100

    HttpClient test

--- app/src/test/groovy/net/pryszawa/micronautlab/app/MicronautLabSpec.groovy
+++ app/src/test/groovy/net/pryszawa/micronautlab/app/MicronautLabSpec.groovy
@@ -1,5 +1,8 @@
 package net.pryszawa.micronautlab.app
 
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import spock.lang.Specification
@@ -11,9 +14,25 @@ class MicronautdemoSpec extends Specification {
     @Inject
     EmbeddedApplication<?> application
 
+    @Inject
+    @Client("/")
+    HttpClient httpClient
+
     void 'First smoke test'() {
         expect:
         application.running
+    }
+
+    void 'Echo'() {
+        given:
+        def request = HttpRequest.GET("/api/diag/echo/Hello%20World?username=pablo53")
+
+        when:
+        def response = httpClient.toBlocking().retrieve(request)
+
+        then:
+        response.contains("Hello World")
+        response.contains("pablo53")
     }
 
 }
